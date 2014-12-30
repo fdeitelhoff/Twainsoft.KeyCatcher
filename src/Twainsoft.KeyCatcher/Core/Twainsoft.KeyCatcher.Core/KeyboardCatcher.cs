d70d163ede135922d09da100782996ea731ee178
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-12-30 22:34:46 +0100

    First version of the KeyboardCatcher class. All key strokes are catched and counted.

---
+++ src/Twainsoft.KeyCatcher/Core/Twainsoft.KeyCatcher.Core/KeyboardCatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+
+namespace Twainsoft.KeyCatcher.Core
+{
+    public class KeyboardCatcher
+    {
+        private KeyboardHookListener KeyboardHookListener { get; set; }
+
+        private long KeyPressCount { get; set; }
+
+        public KeyboardCatcher()
+        {
+            // TODO: Try out the AppHooker class and whats possible with it.
+            KeyboardHookListener = new KeyboardHookListener(new GlobalHooker()) { Enabled = true };
+            KeyboardHookListener.KeyPress += KeyboardHookListenerOnKeyPress;
+
+            KeyPressCount = 0;
+        }
+
+        private void KeyboardHookListenerOnKeyPress(object sender, KeyPressEventArgs keyPressEventArgs)
+        {
+            KeyPressCount++;
+
+            Console.WriteLine(KeyPressCount);
+        }
+    }
+}
---
+++ src/Twainsoft.KeyCatcher/GUI/Twainsoft.KeyCatcher.GUI/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using Twainsoft.KeyCatcher.Core;
+
+namespace Twainsoft.KeyCatcher.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            new KeyboardCatcher();
+        }
+    }
+}
