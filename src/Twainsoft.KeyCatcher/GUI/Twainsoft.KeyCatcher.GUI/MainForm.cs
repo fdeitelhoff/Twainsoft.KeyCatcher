8ed6eacafcce96c8b857bc9a16d66cf018ae03e2
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2015-01-01 23:16:04 +0100

    More logic for handling keys. Refined the notification icon. Ability to re open the form with a double click on the tray icon.

--- src/Twainsoft.KeyCatcher/GUI/Twainsoft.KeyCatcher.GUI/MainForm.cs
+++ src/Twainsoft.KeyCatcher/GUI/Twainsoft.KeyCatcher.GUI/MainForm.cs
@@ -1,35 +1,41 @@
 ﻿using System;
 using System.Windows.Forms;
 using Twainsoft.KeyCatcher.Core;
+using Twainsoft.KeyCatcher.Core.Keyboard;
 
 namespace Twainsoft.KeyCatcher.GUI
 {
     public partial class MainForm : Form
     {
+        private KeyboardCatcher KeyboardCatcher { get; set; }
+
         public MainForm()
         {
             InitializeComponent();
 
-            new KeyboardCatcher();
+            KeyboardCatcher = new KeyboardCatcher();
+            KeyboardCatcher.KeyStroked += KeyboardCatcherOnKeyStroked;
         }
 
         private void notifyIcon_DoubleClick(object sender, EventArgs e)
         {
-
+            Show();
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
             {
-                notifyIcon.Visible = true;
                 notifyIcon.ShowBalloonTip(500);
                 Hide();
             }
-            else if (WindowState == FormWindowState.Normal)
-            {
-                notifyIcon.Visible = false;
-            }
+        }
+
+        private void KeyboardCatcherOnKeyStroked(object sender, KeyStrokeEventArgs keyStrokeEventArgs)
+        {
+            keyStrokeCount.Text = string.Format("Current Key Strokes: {0}", keyStrokeEventArgs.KeyStrokes);
         }
     }
 }
