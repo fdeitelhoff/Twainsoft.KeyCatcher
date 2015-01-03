a704a708a16cdecea7be283d61fb36e29e8a914f
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2015-01-03 17:25:41 +0100

    More special keys added to the KeyUp handler. Some refactorings concerning the special key handling.

--- src/Twainsoft.KeyCatcher/Core/Twainsoft.KeyCatcher.Core/Keyboard/KeyboardCatcher.cs
+++ src/Twainsoft.KeyCatcher/Core/Twainsoft.KeyCatcher.Core/Keyboard/KeyboardCatcher.cs
@@ -49,13 +49,34 @@ namespace Twainsoft.KeyCatcher.Core
         private void KeyboardHookListenerOnKeyUp(object sender, KeyEventArgs keyEventArgs)
         {
             // Catch all keys that are not handled by the KeyPressed event.
-            if (keyEventArgs.KeyValue >= 37 && keyEventArgs.KeyValue <= 40) // Arrows
+            if (CheckKeyValues(keyEventArgs))
             {
                 OnKeyStroked();
 
                 Console.WriteLine("KeyUp: " + DateTime.Now.ToString("hh: mm:ss.FFFFFFF") + "\t\t\t" +
-                                  keyEventArgs.KeyData);
+                                  keyEventArgs.KeyData + "  (" + KeyPressCount + ")  " + keyEventArgs.KeyValue);
             }
+            else
+            {
+                Console.WriteLine("KeyUp currently not handled: " + DateTime.Now.ToString("hh: mm:ss.FFFFFFF") +
+                                  "\t\t\t" +
+                                  keyEventArgs.KeyData + "  " + keyEventArgs.KeyValue);
+            }
+        }
+
+        private bool CheckKeyValues(KeyEventArgs keyEventArgs)
+        {
+            // TODO: Maybe this can be combined in the future or expressed in completely a different way.
+            return 
+                ((keyEventArgs.KeyValue >= 19 && keyEventArgs.KeyValue <= 20)           // Pause, Capital
+                || (keyEventArgs.KeyValue >= 33 && keyEventArgs.KeyValue <= 40)         // PageUp, Next, End, Home, Arrows (Left, Up, Right, Down)
+                || (keyEventArgs.KeyValue >= 44 && keyEventArgs.KeyValue <= 46)         // PrintScreen, Insert, Delete
+                || (keyEventArgs.KeyValue >= 48 && keyEventArgs.KeyValue <= 57)         // ALT + 1..0 => \ ²³{[]} (not all keys are assigned)
+                || (keyEventArgs.KeyValue >= 160 && keyEventArgs.KeyValue <= 164)       // Left-Shift, Right-Shift, Left-Control, Right-Control, Alt
+                || (keyEventArgs.KeyValue >= 112 && keyEventArgs.KeyValue <= 123)       // F1..F12
+                || (keyEventArgs.KeyValue >= 219 && keyEventArgs.KeyValue <= 221)       // \ (OemOpenBrackets), ^ and °, ´ and `
+                || keyEventArgs.KeyValue == 12 || keyEventArgs.KeyValue == 91           // Clear, Left-Windows
+                || keyEventArgs.KeyValue == 144);                                       // NumLock
         }
 
         protected virtual void OnKeyStroked()
