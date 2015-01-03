925d2eedb383a79a1b4fe2dfaaa37faf7fa1cf82
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2015-01-03 17:40:54 +0100

    Fixed a bug within the KeyValue-Checker (ALT + 48..57). Some refactorings within the KeyboardCatcher class.

--- src/Twainsoft.KeyCatcher/Core/Twainsoft.KeyCatcher.Core/Keyboard/KeyboardCatcher.cs
+++ src/Twainsoft.KeyCatcher/Core/Twainsoft.KeyCatcher.Core/Keyboard/KeyboardCatcher.cs
@@ -1,41 +1,32 @@
 ﻿using System;
-using System.Globalization;
 using System.Windows.Forms;
 using MouseKeyboardActivityMonitor;
 using MouseKeyboardActivityMonitor.WinApi;
-using Twainsoft.KeyCatcher.Core.Keyboard;
 
-namespace Twainsoft.KeyCatcher.Core
+namespace Twainsoft.KeyCatcher.Core.Keyboard
 {
-    public class KeyboardCatcher
+    public sealed class KeyboardCatcher
     {
         private KeyboardHookListener KeyboardHookListener { get; set; }
 
         private long KeyPressCount { get; set; }
 
-        public delegate void KeyStrokeEventHandler(object sender, KeyStrokeEventArgs e);
-
         public event KeyStrokeEventHandler KeyStroked;
+        public delegate void KeyStrokeEventHandler(object sender, KeyStrokeEventArgs e);
 
         public KeyboardCatcher()
         {
-            // TODO: Try out the AppHooker class and whats possible with it.
-            KeyboardHookListener = new KeyboardHookListener(new GlobalHooker()) { Enabled = true };
-            // TODO: Those key strokes are not necessary in the correct order.
-            // TODO: KeyDown and KeyUp are good to catch modifiers or sort of keys.
-            // Temporary remove of KeyDown. I think KeyUp for modifiers and so on will work.
-            //KeyboardHookListener.KeyDown += KeyboardHookListenerOnKeyDown;
-            KeyboardHookListener.KeyUp += KeyboardHookListenerOnKeyUp;
-            // TODO: This is a nice event, but will not handle non-character keys and thats bad!
+            KeyboardHookListener = new KeyboardHookListener(new GlobalHooker())
+            {
+                Enabled = true
+            };
+
+            // We need the KeyPress and KeyUp events for normal and special keys.
             KeyboardHookListener.KeyPress += KeyboardHookListenerOnKeyPress;
+            KeyboardHookListener.KeyUp += KeyboardHookListenerOnKeyUp;
 
             KeyPressCount = 0;
         }
-
-        /*private void KeyboardHookListenerOnKeyDown(object sender, KeyEventArgs keyEventArgs)
-        {
-            Console.WriteLine("KeyDown: " + DateTime.Now.ToString("hh: mm:ss.FFFFFFF") + "\t\t\t" + keyEventArgs.KeyData);
-        }*/
 
         private void KeyboardHookListenerOnKeyPress(object sender, KeyPressEventArgs keyPressEventArgs)
         {
@@ -49,7 +40,7 @@ namespace Twainsoft.KeyCatcher.Core
         private void KeyboardHookListenerOnKeyUp(object sender, KeyEventArgs keyEventArgs)
         {
             // Catch all keys that are not handled by the KeyPressed event.
-            if (CheckKeyValues(keyEventArgs))
+            if (CheckKeyValueRanges(keyEventArgs))
             {
                 OnKeyStroked();
 
@@ -64,14 +55,14 @@ namespace Twainsoft.KeyCatcher.Core
             }
         }
 
-        private bool CheckKeyValues(KeyEventArgs keyEventArgs)
+        private bool CheckKeyValueRanges(KeyEventArgs keyEventArgs)
         {
             // TODO: Maybe this can be combined in the future or expressed in completely a different way.
             return 
                 ((keyEventArgs.KeyValue >= 19 && keyEventArgs.KeyValue <= 20)           // Pause, Capital
                 || (keyEventArgs.KeyValue >= 33 && keyEventArgs.KeyValue <= 40)         // PageUp, Next, End, Home, Arrows (Left, Up, Right, Down)
                 || (keyEventArgs.KeyValue >= 44 && keyEventArgs.KeyValue <= 46)         // PrintScreen, Insert, Delete
-                || (keyEventArgs.KeyValue >= 48 && keyEventArgs.KeyValue <= 57)         // ALT + 1..0 => \ ²³{[]} (not all keys are assigned)
+                || (keyEventArgs.Alt && keyEventArgs.KeyValue >= 48 && keyEventArgs.KeyValue <= 57)         // ALT + 1..0 => \ ²³{[]} (not all keys are assigned)
                 || (keyEventArgs.KeyValue >= 160 && keyEventArgs.KeyValue <= 164)       // Left-Shift, Right-Shift, Left-Control, Right-Control, Alt
                 || (keyEventArgs.KeyValue >= 112 && keyEventArgs.KeyValue <= 123)       // F1..F12
                 || (keyEventArgs.KeyValue >= 219 && keyEventArgs.KeyValue <= 221)       // \ (OemOpenBrackets), ^ and °, ´ and `
@@ -79,7 +70,7 @@ namespace Twainsoft.KeyCatcher.Core
                 || keyEventArgs.KeyValue == 144);                                       // NumLock
         }
 
-        protected virtual void OnKeyStroked()
+        private void OnKeyStroked()
         {
             KeyPressCount++;
 
