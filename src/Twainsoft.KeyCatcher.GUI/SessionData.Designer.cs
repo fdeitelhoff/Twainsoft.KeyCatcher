e2139d5aad6e8257e1f5f747d547fc5053268d93
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2015-01-17 22:09:59 +0100

    First version with buttons on the session data window. Not quite sure if that's the right solution.

--- src/Twainsoft.KeyCatcher.GUI/SessionData.Designer.cs
+++ src/Twainsoft.KeyCatcher.GUI/SessionData.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.sessionName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,11 +52,41 @@
             this.sessionName.TabIndex = 1;
             this.sessionName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.sessionName_KeyUp);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(107, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Discard";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // SessionData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 54);
+            this.ClientSize = new System.Drawing.Size(286, 87);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.sessionName);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -72,5 +105,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox sessionName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
